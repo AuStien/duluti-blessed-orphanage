cfd279d9df1d659bc6451f3c739949dc9aa99b1c
Audun Stien <audun97@gmail.com>
2022-05-02 19:05:35 +0200

    add Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+  <div className="navbar-container">
+    <div className="navbar-item">Home</div>
+    <span className="navbar-item">Gallery</span>
+    <span className="navbar-item">Contact</span>
+    <span className="navbar-item">Donate</span>
+  </div>)
+}
