410625fb402adf08658b397b567745bffd1f9925
Audun Stien <audun97@gmail.com>
2022-05-02 19:05:54 +0200

    add Routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router';
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Header />} />
+      </Routes>
     </div>
   );
 }
