dcdce33074449432afd78c1a45e6a830442373c8
Audun Stien <audun97@gmail.com>
2022-05-02 19:05:41 +0200

    add Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar/Navbar";
+
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <h1 className="header-title">Duluti Blessed Orhanage</h1>
+      <Navbar />
+    </div>
+  )
+}
