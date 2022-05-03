1bcead1f5853b957a92e91fd3bc6f2a89f495af3
Audun Stien <audun97@gmail.com>
2022-05-03 19:39:31 +0200

    add Home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router';
 import Header from '../Header/Header';
+import Home from '../Home/Home';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route path='/' element={<Header />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </div>
   );
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,25 @@
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-1">
+        <p>Duluti Blessed Orphanage is a day school for 66 kids in the age between three and seven years old. It gives them a safe environment, education, friendships and a reliable daily meal.</p>
+        <img src="/images/circle.jpg" alt="Children standing in a circle" height="400px" />
+      </div>
+      <div className="home-2">
+        <p>The school was founded by Collins Daniel Makau in 2012, and it is located in an area called Madukania just outside Arusha, Tanzania. He started by renting a schoolhouse in Madukani, and in the beginning he had 10 kids in the age of three to five years old. Collins saw the need for change with the accumulation of children who didn't attend the public school and would spend their days unattended and bored. The idea was to create a school where these children could gain a chance for a better future. This idea was met with support from his friends, family, and community. The result of this is a lovely school where children can begin their learning experience.</p>
+      </div>
+      <div className="home-3">
+        <img src="/images/peace.jpg" alt="Children posing to the camera" height="500px" />
+        <p>Duluti Blessed Orphanage has expanded quickly in size since its opening. In 2013 he started with 20 kids, while this year, 2017, as many as 66 kids attend this school. 29 of the kids are girls and 37 are boys. Out of the 66 kids as many as 30 of them are orphans. The increase in number of kids and kids without parents have lead to the need of a new and larger school house. As a result of this Collins has bought a new ground for a good price where a new school can be built. The next step now is to start building the school.</p>
+      </div>
+      <div className="home-4">
+        <p>The new school will give the kids much more space and a better learning environment. It will also give kids the opportunity of sleeping over at the school as some of the kids have it much better there then at home. To run the school today and to build the new school Collins is dependent on getting in donations. He is already using his savings and selling milk from his cow to pay for everything but this is not enough. Every small donation you give will help these kids get a better life!</p>
+      </div>
+      <div className="home-5">
+        <img src="/images/class-photo.jpg" alt="Class photo" height="900px" />
+      </div>
+    </div>
+  )
+}
